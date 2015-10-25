18a41a59273800153c57731ed9d11190e853545e
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-10-25 02:02:41 +0100

    improve __init__, add reset()

--- ba66.py
+++ ba66.py
@@ -4,10 +4,8 @@ class posdisplay():
     """
     Represent the BA66 display.
     """
-    def __init__(self, port="/dev/ttyUSB0", baudrate=9600):
-        self.port = port
-        self.baudrate = baudrate
-        self.ser = serial.Serial(self.port, self.baudrate, timeout=2, rtscts=1)
+    def __init__(self, port="/dev/ttyUSB0", baudrate=9600, **kwargs):
+        self.ser = serial.Serial(port, baudrate, **kwargs)
 
     def clear(self):
         """ Clear display contents. """
@@ -16,6 +14,11 @@ class posdisplay():
     def position_cursor(self, x, y):
         """ Position the cursor. """
         self.write("\x1B[{0:d};{1:d}H".format(y&0xFF, x&0xFF))
+
+    def reset(self):
+        """ Set cursor to x=0, y=0, and clear the display. """
+        self.clear()
+        self.position_cursor(0, 0)
 
     def write(self, data):
         if isinstance(data, str):
