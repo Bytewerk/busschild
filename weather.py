8276c5867e89e7a851c36164c7997a8c130a94d5
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-10-31 00:47:37 +0100

    clean up weather module

--- weather.py
+++ weather.py
@@ -22,26 +22,6 @@ def get_forecast(city_name, country_code, lang="en", forecast_cnt=3):
     forecast = [current_json] + forecast_json['list']
     return forecast
 
-class Forecast():
-    def __init__(self, fc_dict):
-        self.dt = datetime.fromtimestamp(fc_dict['dt'])
-        self.description = fc_dict['weather'][0]['description']
-        self.temp = fc_dict['main']['temp']
-
-_forecast_fmt = "{:%H:%M} {:>6}C{}"
-_forecast_fmt_min_width = len(_forecast_fmt.format(datetime.fromtimestamp(0), "", ""))
-def fmt_forecasts_sidescrolling(fc_dicts, target_width=20):
-    forecasts = [Forecast(fc_dict) for fc_dict in fc_dicts]
-    max_desc_width = max(map(lambda d: len(d.description), forecasts))
-    target_desc_width = target_width - _forecast_fmt_min_width
-    for forecast in forecasts:
-        forecast.description = forecast.description.ljust(max_desc_width)
-    strs = []
-    scroll_dist = 1 if target_desc_width >= max_desc_width else max_desc_width - target_desc_width + 1
-    for i in range(scroll_dist):
-        strs.append("\r\n".join([_forecast_fmt.format(fc.dt, fc.temp, (fc.description[i:]+fc.description[:i])[:target_desc_width]) for fc in forecasts]))
-    return strs
-
 # def fmt_forecast(forecast):
 #     def __fmt(item):
 #         date_time = datetime.fromtimestamp(item['dt'])
