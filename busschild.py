e805b7fa0e71d6773926cc200851194440901aa2
Peter Brantsch <peter@brantsch.name>
2019-11-12 22:36:35 +0100

    autoformatting

--- busschild.py
+++ busschild.py
@@ -14,14 +14,15 @@ import time
 import traceback
 
 RT_URLS = [
-        "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39099&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly",
-        "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39002&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly",
-        "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39001&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly"
-        ]
+    "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39099&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly",
+    "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39002&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly",
+    "https://fpa.invg.de/bin/stboard.exe/dny?L=vs_liveticker&tpl=liveticker2json&&input=39001&boardType=dep&productsFilter=1111111111&additionalTime=0&disableEquivs=yes&ignoreMasts=yes&maxJourneys=5&start=yes&selectDate=today&monitor=1&outputMode=tickerDataOnly"
+]
 BUS_REFRESH_TIMEOUT = 20
 STOP = "Klinikum"
 DISPLAY_WIDTH = 20
 __SCROLL_SEP = "\xDB"
+
 
 class Bus:
     def __init__(self, date, time, route, destination):
@@ -38,6 +39,7 @@ class Bus:
     def __lt__(self, other):
         return self.datetime < other.datetime
 
+
 def get_realtime_info():
     """
     Fetch realtime information from RT_URLS.
@@ -50,6 +52,7 @@ def get_realtime_info():
     buses.sort()
     return buses
 
+
 def do_departures(display):
     display.reset()
     while True:
@@ -57,22 +60,23 @@ def do_departures(display):
             buses = get_realtime_info()[:4]
             before = datetime.now()
             for bus in buses:
-                if(len(str(bus)) >= DISPLAY_WIDTH):
+                if (len(str(bus)) >= DISPLAY_WIDTH):
                     bus.destination = "{} {} ".format(__SCROLL_SEP, re.sub(r"\s+", " ", bus.destination))
                 else:
                     bus.destination = bus.destination.ljust(DISPLAY_WIDTH - len(str(bus)))
             while (datetime.now() - before).seconds < BUS_REFRESH_TIMEOUT:
-                    display.position_cursor(0,0)
-                    display_cmds = '\r\n'.join(map(lambda b: str(b)[:20], buses))
-                    display.write(display_cmds)
-                    for bus in buses:
-                        if(len(str(bus)) >= DISPLAY_WIDTH):
-                            bus.destination = bus.destination[1:]+bus.destination[:1]
-                    time.sleep(0.5)
+                display.position_cursor(0, 0)
+                display_cmds = '\r\n'.join(map(lambda b: str(b)[:20], buses))
+                display.write(display_cmds)
+                for bus in buses:
+                    if (len(str(bus)) >= DISPLAY_WIDTH):
+                        bus.destination = bus.destination[1:] + bus.destination[:1]
+                time.sleep(0.5)
         except:
             display.reset()
             display.write("Keine Daten od. Feh-\r\nler. Lauf heim.")
             time.sleep(BUS_REFRESH_TIMEOUT)
+
 
 def main():
     while True:
@@ -85,6 +89,7 @@ def main():
         except:
             print(traceback.format_exc(), file=sys.stderr)
             time.sleep(10)
+
 
 if __name__ == '__main__':
     main()
